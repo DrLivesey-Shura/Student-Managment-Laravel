e3deb84200f56b687c5eabc844bca329a6e89e4c
Merah Alaeddine <aladinemire@gmail.com>
2025-02-17 18:36:17 +0100

    adding roles (user/admin)

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Students List</h1>
-        <a href="{{ route('students.create') }}" class="btn btn-primary mb-3">Add New Student</a>
-
+        @if (auth()->user()->role === 'admin')
+            <a href="{{ route('students.create') }}" class="btn btn-primary mb-3">Add New Student</a>
+        @endif
         <table class="table table-bordered table-striped">
             <thead class="table-dark">
                 <tr>
@@ -36,13 +37,16 @@
                         </td>
                         <td>
                             <a href="{{ route('students.show', $student->id) }}" class="btn btn-info btn-sm">View</a>
-                            <a href="{{ route('students.edit', $student->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                            <form action="{{ route('students.destroy', $student->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm"
-                                    onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>
+                            @if (auth()->user()->role === 'admin')
+                                <a href="{{ route('students.edit', $student->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                <form action="{{ route('students.destroy', $student->id) }}" method="POST"
+                                    class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                        onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>
+                            @endif
                         </td>
                     </tr>
                 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,21 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware('auth')->group(function () {
-    Route::resource('students', StudentController::class);
-});
+// Route::middleware('auth')->group(function () {
+//     Route::resource('students', StudentController::class);
+// });
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+    Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+});
